b2ed901d65c07006f9bf2d13ef3c030c2381f32f
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-04-17 09:12:34 +0500

    app remove main.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 import Navbar from "./Components/Header/Header";
-import Main from "./Components/Mains/main";
 import Main1 from "./Components/Mains/main1/Main1";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Main />
       <Main1 />
     </div>
   );
